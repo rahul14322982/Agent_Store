2ba7a611836b10aed60b5dceb04a509e59b34707
rahul k.s <rahulks14322@gmail.com>
2025-10-07 10:06:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from fastapi import FastAPI
 from api_agents import router as agent_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(titel="Agent health-Demo")
 app.include_router(agent_router, prefix="/agents", tags=["agents"])
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # allow all origins
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
